1c9f487e8bb10b744f9e6626ec3c04be00c6ee92
P3PiT0 <lucas.chz@laposte.net>
2023-05-16 18:48:37 -0400

    Update G-code_Editor.py

--- G-code_Editor.py
+++ G-code_Editor.py
@@ -1,16 +1,20 @@
 import string 
 
-new_code=""
+new_code=""  
+#print(code[len(code)-2])   
 
-with open("xyz-10mm-calibration-cube_0.4n_0.2mm_PLA_MK4_8m.gcode", 'r') as f:
-    code=list(map(str,f.read().split("\n")))
+
+def Main() :
+    #Interraction avec l'utilisateur 
     
-    #print(code[len(code)-2])   
+         
+    #Extraction du G-code inital et séparation des lignes
+    with open("xyz-10mm-calibration-cube_0.4n_0.2mm_PLA_MK4_8m.gcode", 'r') as f:
+        code=list(map(str,f.read().split("\n")))
     
-if code[i][0] == ";": #Cette ligne est un commentaire, on le recopie simplement 
-    new_code += code
+    if code[i][0] == ";": #Cette ligne est un commentaire, on le recopie simplement 
+        new_code += code 
        
-    
-
-with open("new-xyz-10mm-calibration-cube_0.4n_0.2mm_PLA_MK4_8m.gcode", "w") as fichier:
-    fichier.write(new_code)
+    #Ecriture du G-code mis à jour dans un nouveau fichier
+    with open("new-xyz-10mm-calibration-cube_0.4n_0.2mm_PLA_MK4_8m.gcode", "w") as fichier:
+        fichier.write(new_code)
